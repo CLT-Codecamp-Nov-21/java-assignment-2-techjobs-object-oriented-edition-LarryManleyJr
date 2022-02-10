ca258463515da641ae9afa24c217c2b5e23fd911
Notorious Nerd <NotoriousNerd@The-11-Creative.local>
2022-02-10 17:07:51 -0500

    number4

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -1,12 +1,34 @@
 package org.launchcode.techjobs.oo.test;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+import static org.junit.Assert.*;
+
+
 
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
+
+    @Test
+    public void testSettingJobId () {
+    JobTest firstJob = new JobTest();
+    JobTest secondJob = new JobTest();
+    assertNotEquals(firstJob, secondJob);
+    }
+
+
+    @Test
+    public void testJobsForEquality() {
+
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+
+    }
 
 }
