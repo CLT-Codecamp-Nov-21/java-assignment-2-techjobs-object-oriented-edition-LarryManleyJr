e4706793ad9089a14fb1a93179ba240c827a05ba
Notorious Nerd <NotoriousNerd@The-11-Creative.local>
2022-02-09 22:34:15 -0500

    test2complete

--- src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
+++ src/main/java/org/launchcode/techjobs/oo/CoreCompetency.java
@@ -9,7 +9,7 @@ public class CoreCompetency {
     private String value;
 
     public CoreCompetency() {
-        this.id = nextId;
+        id = nextId;
         nextId++;
     }
 
@@ -30,15 +30,27 @@ public class CoreCompetency {
         if (this == o) return true;
         if (!(o instanceof CoreCompetency)) return false;
         CoreCompetency that = (CoreCompetency) o;
-        return id == that.id;
+        return getId() == this.getId();
+
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);
+        return Objects.hash(getId());
     }
 
     // TODO: Use the "Generate" tool to add a getter and setter for the 'value' field but
     //  ONLY a getter for the 'id' field.
 
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
